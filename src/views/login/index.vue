cb3cdd4c53341422976dad85e4f27dbafc21d71b
nwa <2428284043@qq.com>
2024-03-06 08:37:40 +0800

    feat: 新增全局引入icon

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,49 +1,47 @@
 <template>
+  <el-button type="primary" size="small" icon="plus">
+    <!-- <i-ep-edit /> -->
+    编辑2种自动引入的方法应用
+  </el-button>
+
+  <hr />
+  默认是英文的，需要main那修改配置
+  <el-pagination
+    :page-size="100"
+    layout="total, prev, pager, next"
+    :total="1000"
+  />
+  <hr />
+
+  <h1>测试svg,用xlink:herf执行哪一个图标#icon-图标名称</h1>
+  <svg style="width: 30px; height: 30px">
+    <use xlink:href="#icon-phone" fill="pink"></use>
+  </svg>
+  <SvgIcon name="phone" color="blue"></SvgIcon>
+  <div class="testColor">lucky小维</div>
+
+  <hr />
+  <div>{{ luckyC }}</div>
+  <hr />
   <div>
-    <el-button type="primary" size="small">
-      <i-ep-edit />
-      编辑
-    </el-button>
-
-    <hr />
-    默认是英文的，需要main那修改配置
-    <el-pagination
-      :page-size="100"
-      layout="total, prev, pager, next"
-      :total="1000"
-    />
-    <hr />
-
-    <h1>测试svg,用xlink:herf执行哪一个图标#icon-图标名称</h1>
-    <svg style="width: 30px; height: 30px">
-      <use xlink:href="#icon-phone" fill="pink"></use>
-    </svg>
-    <SvgIcon name="phone" color="blue"></SvgIcon>
-    <div class="testColor">lucky小维</div>
-
-    <hr />
-    <div>{{ luckyC }}</div>
-    <hr />
-    <div>
-      hooks混入,也很好用：点击变化，当前x的值：{{ x }},当前y的值是：{{ y }}
-    </div>
-    <hr />
-    <TestA />
-    <hr />
-    这是elementui图标按需导入也不要注册,不过有前缀
-    <i-ep-add-location />
-    <i-ep-edit />
-    <hr />
-    这是elementui组件按需自动导入
-    <el-button :plain="true" @click="open">Show message</el-button>
-    <hr />
-    这里测试mockjs模拟的数据
-    <el-button :plain="true" @click="login">登录</el-button>
-
-    <el-button :plain="true" @click="iTest">牛逼i18n测试</el-button>
-
-    <hr />
+    hooks混入,也很好用：点击变化，当前x的值：{{ x }},当前y的值是：{{ y }}
   </div>
+  <hr />
+  <TestA />
+  <hr />
+  这是elementui图标按需导入也不要注册,不过有前缀
+  <i-ep-add-location />
+  <i-ep-edit />
+  <hr />
+  这是elementui组件按需自动导入
+  <el-button :plain="true" @click="open">Show message</el-button>
+  <hr />
+  这里测试mockjs模拟的数据
+  <el-button :plain="true" @click="login">登录</el-button>
+
+  <el-button :plain="true" @click="iTest">牛逼i18n测试</el-button>
+
+  <hr />
 </template>
 
 <script setup lang="ts">
